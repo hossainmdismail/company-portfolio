28a38b44aeefb337182bcd2f409e01cc03bed48f
hossainmdismail <hossainmdismail9010@gmail.com>
2023-09-16 01:11:29 +0600

    Done

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
