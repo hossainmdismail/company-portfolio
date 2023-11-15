256ff1549ab1d5244b2331cb7035eff6e9019f1c
esmail <120841700+hossainmdismail@users.noreply.github.com>
2023-11-16 04:39:22 +0600

    0.4

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,8 +9,12 @@
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-sm text-center">
-                <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
-                    404</h1>
+                {{-- <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+                    404</h1> --}}
+                <div class="w-full flex items-center justify-center">
+
+                    <img class="w-72" src="{{ asset('default/404.png') }}" alt="">
+                </div>
                 <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's
                     missing.</p>
                 <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll
